8eacc823bba2f846e44acfd2a855e92dd7db0f81
UmiKami <ggoku02@gmail.com>
2023-02-22 13:20:39 -0500

    init files

---
+++ src/Components/VideoRating.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VideoRating = () => {
+  return (
+    <div>VideoRating</div>
+  )
+}
+
+export default VideoRating
---
+++ src/Views/LandingPage.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+const LandingPage = () => {
+
+    return (
+        <div>
+            landing page
+        </div>
+    );
+};
+
+export default LandingPage;
