4d54769dc12d97b26a2ed5506f44c91d98e3f87a
denny <denny>
2023-03-01 15:58:29 -0500

    updates for video and multiple videos(feed)

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -23,7 +23,7 @@ const Layout = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/feed" element={<VideoFeed />} />
-                <Route path="/watch" element={<VideoPage />} />
+                <Route path="/watch/:id" element={<VideoPage />} />
                 <Route path="/profile" element={<UserProfile />} />
                 <Route path="/upload" element={<VideoUpload setloggedIn={setloggedIn} />} />
             </Routes>
