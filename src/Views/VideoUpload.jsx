cf1b43dc60244fdf2f3c929cd29ea4c476095283
denny <denny>
2023-03-01 12:24:03 -0500

    format of page and cancel button reroute to /feed page

--- src/Views/VideoUpload.jsx
+++ src/Views/VideoUpload.jsx
@@ -5,6 +5,7 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
 import axios from "axios";
 import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 
 function VideoUpload({setloggedIn}) {
     const { register, handleSubmit } = useForm();
@@ -54,31 +55,8 @@ function VideoUpload({setloggedIn}) {
         return (
         <div>
             <p>Video Upload Page</p>
-            <span className="border border-info" >
-                Difficulty            
-                <input type="radio" value="beginner" id="beginner" />
-                <label htmlFor="beginner">Beginner</label>
-                <input type="radio"  value="intermediate" id="intermediate" />
-                <label htmlFor="intermediate">Intermediate</label>
-                <input type="radio"  value="advanced" id="advanced" />
-                <label htmlFor="advanced">Advanced</label>
-            </span>
-
-            <h3>Category</h3>
-            <select>
-                <option value="ashtanga">Ashtanga yoga (eight-limbed yoga)</option>
-                <option value="hatha">Hatha yoga (force yoga)</option>
-                <option value="hot">Hot yoga (hot room yoga)</option>
-                <option value="iyengar">Iyengar yoga (focuses on three aspects: alignment, sequencing, and timing )</option>
-                <option value="kundalini">Kundalini yoga (yoga of awareness)</option>
-                <option value="power">Power yoga (build strength and endurance)</option>
-                <option value="restorative">Restorative yoga (relaxing, calming, and healing effects)</option>
-                <option value="vinyasa">Vinyasa yoga (flow yoga)</option>
-            </select>
-            <button type = "submit">Cancel</button>
-            <button type = "submit">Submit</button>
-
             <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="file" {...register("file")} />
                 <TextField
                     label="Title"
                     variant="outlined"
@@ -95,9 +73,31 @@ function VideoUpload({setloggedIn}) {
                             onChange={(e) => setVideoFile(e.target.files)}
                             
                         /> */}
-                <input type="file" {...register("file")} />
-                <input type="submit" />
+            <span className="border border-info" >
+                <h5>Difficulty</h5>
+                <input type="radio" value="beginner" id="beginner" />
+                <label htmlFor="beginner">Beginner</label>
+                <input type="radio"  value="intermediate" id="intermediate" />
+                <label htmlFor="intermediate">Intermediate</label>
+                <input type="radio"  value="advanced" id="advanced" />
+                <label htmlFor="advanced">Advanced</label>
+            </span>
+
+            <h5>Category</h5>
+            <select>
+                <option value="ashtanga">Ashtanga yoga (eight-limbed yoga)</option>
+                <option value="hatha">Hatha yoga (force yoga)</option>
+                <option value="hot">Hot yoga (hot room yoga)</option>
+                <option value="iyengar">Iyengar yoga (focuses on three aspects: alignment, sequencing, and timing )</option>
+                <option value="kundalini">Kundalini yoga (yoga of awareness)</option>
+                <option value="power">Power yoga (build strength and endurance)</option>
+                <option value="restorative">Restorative yoga (relaxing, calming, and healing effects)</option>
+                <option value="vinyasa">Vinyasa yoga (flow yoga)</option>
+            </select>  
+               <button type ="submit"> <input type="submit" /></button>
+               
             </form>
+            <button type ="" ><Link to="/feed">Cancel</Link> </button>
         </div>
     )
 }
