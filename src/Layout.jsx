fd1d78fc11b8c1001dff8e0284c05bf9001d7c20
estephanysanchez <estephany.sanchezcriado@strategio.tech>
2023-02-28 21:43:02 -0500

    finishing signup and login views

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,15 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import LandingPage from "./Views/LandingPage";
 import Login from "./Views/Login";
 import NoPage from "./Views/NoPage";
 import SignUp from "./Views/SignUp";
+import VideoFeed from "./Views/VideoFeed"
 import UserProfile from "./Views/UserProfile";
 import VideoPage from "./Views/VideoPage";
 import './Layout.css';
+import { useEffect } from "react";
 
 
 const Layout = () => {
+
+   
     return (
         <BrowserRouter>
             <Navbar/>
@@ -18,6 +22,7 @@ const Layout = () => {
                 <Route path="/home" element={<LandingPage />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
+                <Route path="/feed" element={<VideoFeed />} />
                 <Route path="/watch" element={<VideoPage />} />
                 <Route path="/profile" element={<UserProfile />} />
                 <Route path="*" element={<NoPage />} />
--- src/Views/Login.jsx
+++ src/Views/Login.jsx
@@ -1,17 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { Button } from 'react-bootstrap';
 import backimage from '../assets/background.jpg'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+
   const [userName, setUserName] = useState('');
   const [password, setPass] = useState('');
+  const navigate = useNavigate()
+
+
+  useEffect(() => {
+      if(localStorage.getItem("token")){
+          navigate("/feed")
+      }
+  }, [localStorage.getItem("token") ])
+
 
   const handleSubmit = (e) => {
       e.preventDefault();
-      console.log(userName);
-      console.log(password);
-  }
+      axios
+      .post("https://api.serenitystream.tv/api/v2/auth/login", {
+        username: userName,
+        password: password
+       })
+       .then((res) => {
+        localStorage.setItem("token", res.data.AuthenticationResult.AccessToken)
+        navigate('/feed')
+      })
+      .catch((err) => { 
+       
+          alert("Wrong User Name or Password", err);
+            
+      });
+      //console.log(userName);
+      // console.log(password);
+      //dispatch(login(userName, password))
+      
+  };
 
   const formStyle={
     textAlign: 'center', 
@@ -26,11 +54,14 @@ const Login = () => {
 
 
   return (
+    
   
     <div className="d-flex justify-content-center align-items-center" style={{backgroundSize: 'cover', height:'100vh'}}>
         
         <img src={backimage} alt="" style={{ objectFit:'cover', height: '100%', width: '100%', opacity:' 0.55 ', position:'absolute',zIndex:'-500'}}  />
+        
             <div className="col-12 d-flex flex-column align-items-center" >
+           
                 <h1 style={{textAlign: 'center',padding: '3%', color:'#014E58', top: '58px',fontFamily: 'Raleway',fontWeight: '700',fontSize: '48px'}}>Log In</h1>
   
                 <form className="col-12 col-sm-7 col-md-5" onSubmit={handleSubmit} style={formStyle}>         
@@ -41,8 +72,12 @@ const Login = () => {
                 
                 </form>
             </div>
- </div>
+      </div>
+      
+ 
   )
 }
 
 export default Login
+
+
