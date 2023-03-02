93f71fb1994d0c2a1f2f3b0495a366f669155d05
UmiKami <ggoku02@gmail.com>
2023-03-02 01:49:49 -0500

    prevents double click

--- src/Components/VideoRating.jsx
+++ src/Components/VideoRating.jsx
@@ -21,18 +21,29 @@ const getLabelText = (value) => {
   return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
 }
 
-const VideoRating = () => {
+const VideoRating = ({id}) => {
   const [value, setValue] = React.useState(2);
   const [hover, setHover] = React.useState(-1);
+  const [flag, setFlag] = React.useState(true)
+  
+  const handleRate = () => {
+  flag && console.log("click");
+  flag && axios
+      .post(
+          `https://api.serenitystream.tv/api/v2/videos/${id}/rate/${Math.round(
+              value
+          )}`
+      )
+      .then((res) => {
+          let rating = Math.round(res.data.rating);
+          setValue(rating);
+      })
+      .catch((err) => {
+          console.log(err);
+      });
+      setFlag(false)
+  }
 
-  axios.post(
-      `https://api.serenitystream.tv/api/v2/videos/1e9d07f7-7d3c-4e0a-acab-453d9cb089e2/rate/${Math.round(value)}`
-  ).then(res => {
-    let rating = Math.round(res.data.rating);
-    setValue(rating)
-  }).catch(err =>{
-    console.log(err);
-  })
 
   return (
     <Box
@@ -45,10 +56,12 @@ const VideoRating = () => {
       <Rating
         name="hover-feedback"
         value={value}
-        precision={0.5}
+        precision={1}
         getLabelText={getLabelText}
+        onClick={()=>handleRate()}
         onChange={(event, newValue) => {
           setValue(newValue);
+          setFlag(true)
         }}
         emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
       />
